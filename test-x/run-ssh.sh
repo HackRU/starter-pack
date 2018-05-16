5f7df40ca81cdab32eada718a0283419cb24f019
Ubuntu <ubuntu@ip-172-31-16-45.us-west-2.compute.internal>
2018-05-16 01:07:02 +0000

    minor tweaks

--- test-x/run-ssh.sh
+++ test-x/run-ssh.sh
@@ -1,12 +1,15 @@
 
-docker build -t ssh-test git://github.com/rogaha/docker-desktop.git
-CONTAINER_ID=$(docker run -d -P ssh-test)
+#build image if not there
+(docker image ls | grep ssh-test) || (docker build -t ssh-test git://github.com/rogaha/docker-desktop.git)
+#run image, putting first argument as port. Empty string is OK: docker will just pick a port
+CONTAINER_ID=$(docker run -d -p $1:22 ssh-test)
 echo "Credentials: $(docker logs $CONTAINER_ID | sed -n 1p)"
 PORT=$(docker port $CONTAINER_ID 22)
 echo "External port: $PORT"
 
 echo "Starting session..."
-IP_ADDR = $(ifconfig | grep "inet addr:" | cut -f 1 | cut -d: -f 2)
+IP_ADDR = "$(hostname | sed -e 's/ip/ec2/').us-west-2.compute.amazonaws.com"
 echo $IP_ADDR
 DESKTOP_SESS_NUM=10
 ssh docker@$IPADDR -p $PORT "sh -c './docker-desktop -s 800x600 -d $DESKTOP_SESS_NUM > /dev/null 2>&1 &'"
+echo "run:\nxpra --ssh=\"ssh -p $PORT\" attach ssh:docker@$IP_ADDR:$DESKTOP_SESS_NUM"
